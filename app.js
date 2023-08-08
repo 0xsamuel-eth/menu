0eb6aef24882a444b57308b49f2377f20f3dcf70
0xsamuel-eth <sammmastenn@gmail.com>
2023-08-08 16:55:20 -0400

    Added filterBtns const with EventListener to run dataset on clicks

--- app.js
+++ app.js
@@ -75,10 +75,24 @@ const menu = [
 ];
 
 const sectionCenter = document.querySelector(".section-center");
+const filterBtns = document.querySelectorAll(".filter-btn");
 
+// load items
 window.addEventListener("DOMContentLoaded",function() {
   displayMenuItems(menu);
 });
+// filter items
+filterBtns.forEach(function(btn) {
+  btn.addEventListener("click", function(e) {
+    console.log(e.currentTarget.dataset);
+  });
+});
+
+
+
+
+
+
 
 function displayMenuItems(menuItems) {
   let displayMenu = menuItems.map(function(item) {
